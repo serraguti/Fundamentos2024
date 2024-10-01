cfe81c9bac05e07185065a614a88e1df888f0cb4
serraguti <pacoserranox@gmail.com>
2024-10-01 19:30:12 +0200

    cccc

--- Fundamentos2024/Form05SumarTextoNumeros.cs
+++ Fundamentos2024/Form05SumarTextoNumeros.cs
@@ -21,7 +21,20 @@ namespace Fundamentos2024
         {
             //CAPTURAMOS EL TEXTO DE LA CAJA DE NUMEROS
             string textoSuma = this.txtNumeros.Text;
-
+            int suma = 0;
+            //RECORREMOS TODOS LOS CARACTERES DEL TEXTO
+            //DE UNO EN UNO
+            for (int i = 0; i < textoSuma.Length; i++)
+            {
+                //CAPTURAMOS CADA CARACTER DEL TEXTO
+                char caracter = textoSuma[i]; //"1", "2"
+                //DEBEMOS CONVERTIR EL CARACTER A NUMERO
+                int numero = int.Parse(caracter.ToString());
+                //SUMAMOS CADA CARACTER A LA VARIABLE suma
+                suma = suma + numero;
+            }
+            //DIBUJAMOS LA SUMA EN EL LABEL DE RESULTADO
+            this.lblResultado.Text = suma.ToString();
         }
     }
 }
