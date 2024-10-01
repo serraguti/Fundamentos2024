a94e7b9a6c9b5d0d4214b1579259ed642ad8d247
serraguti <pacoserranox@gmail.com>
2024-10-01 21:09:51 +0200

    commit

---
+++ Fundamentos2024/Form07ColeccionGrafica.Designer.cs
@@ -0,0 +1,156 @@
+﻿namespace Fundamentos2024
+{
+    partial class Form07ColeccionGrafica
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            lstElementos = new ListBox();
+            label2 = new Label();
+            txtElemento = new TextBox();
+            btnInsertar = new Button();
+            btnEliminar = new Button();
+            btnBorrarTodo = new Button();
+            lblItemSeleccionado = new Label();
+            lblIndiceSeleccionado = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(30, 28);
+            label1.Name = "label1";
+            label1.Size = new Size(109, 30);
+            label1.TabIndex = 0;
+            label1.Text = "Elementos";
+            // 
+            // lstElementos
+            // 
+            lstElementos.FormattingEnabled = true;
+            lstElementos.ItemHeight = 30;
+            lstElementos.Location = new Point(38, 65);
+            lstElementos.Name = "lstElementos";
+            lstElementos.Size = new Size(267, 244);
+            lstElementos.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(389, 21);
+            label2.Name = "label2";
+            label2.Size = new Size(167, 30);
+            label2.TabIndex = 2;
+            label2.Text = "Nuevo elemento";
+            // 
+            // txtElemento
+            // 
+            txtElemento.Location = new Point(392, 54);
+            txtElemento.Name = "txtElemento";
+            txtElemento.Size = new Size(238, 35);
+            txtElemento.TabIndex = 3;
+            // 
+            // btnInsertar
+            // 
+            btnInsertar.Location = new Point(389, 109);
+            btnInsertar.Name = "btnInsertar";
+            btnInsertar.Size = new Size(241, 49);
+            btnInsertar.TabIndex = 4;
+            btnInsertar.Text = "Insertar";
+            btnInsertar.UseVisualStyleBackColor = true;
+            // 
+            // btnEliminar
+            // 
+            btnEliminar.ForeColor = Color.Red;
+            btnEliminar.Location = new Point(387, 176);
+            btnEliminar.Name = "btnEliminar";
+            btnEliminar.Size = new Size(243, 53);
+            btnEliminar.TabIndex = 5;
+            btnEliminar.Text = "Eliminar";
+            btnEliminar.UseVisualStyleBackColor = true;
+            // 
+            // btnBorrarTodo
+            // 
+            btnBorrarTodo.Location = new Point(389, 245);
+            btnBorrarTodo.Name = "btnBorrarTodo";
+            btnBorrarTodo.Size = new Size(241, 50);
+            btnBorrarTodo.TabIndex = 6;
+            btnBorrarTodo.Text = "Borrar todo";
+            btnBorrarTodo.UseVisualStyleBackColor = true;
+            // 
+            // lblItemSeleccionado
+            // 
+            lblItemSeleccionado.AutoSize = true;
+            lblItemSeleccionado.Location = new Point(38, 327);
+            lblItemSeleccionado.Name = "lblItemSeleccionado";
+            lblItemSeleccionado.Size = new Size(199, 30);
+            lblItemSeleccionado.TabIndex = 7;
+            lblItemSeleccionado.Text = "lblItemSeleccionado";
+            // 
+            // lblIndiceSeleccionado
+            // 
+            lblIndiceSeleccionado.AutoSize = true;
+            lblIndiceSeleccionado.Location = new Point(38, 370);
+            lblIndiceSeleccionado.Name = "lblIndiceSeleccionado";
+            lblIndiceSeleccionado.Size = new Size(213, 30);
+            lblIndiceSeleccionado.TabIndex = 8;
+            lblIndiceSeleccionado.Text = "lblIndiceSeleccionado";
+            // 
+            // Form07ColeccionGrafica
+            // 
+            AutoScaleDimensions = new SizeF(12F, 30F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(673, 429);
+            Controls.Add(lblIndiceSeleccionado);
+            Controls.Add(lblItemSeleccionado);
+            Controls.Add(btnBorrarTodo);
+            Controls.Add(btnEliminar);
+            Controls.Add(btnInsertar);
+            Controls.Add(txtElemento);
+            Controls.Add(label2);
+            Controls.Add(lstElementos);
+            Controls.Add(label1);
+            Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            Margin = new Padding(5, 6, 5, 6);
+            Name = "Form07ColeccionGrafica";
+            Text = "Form07ColeccionGrafica";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private ListBox lstElementos;
+        private Label label2;
+        private TextBox txtElemento;
+        private Button btnInsertar;
+        private Button btnEliminar;
+        private Button btnBorrarTodo;
+        private Label lblItemSeleccionado;
+        private Label lblIndiceSeleccionado;
+    }
+}
--- Fundamentos2024/Program.cs
+++ Fundamentos2024/Program.cs
@@ -11,7 +11,7 @@ namespace Fundamentos2024
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form06ValidarEmail());
+            Application.Run(new Form07ColeccionGrafica());
         }
     }
 }
