a0fa19844ff0a037fc238199e6b0098822aec73b
serraguti <pacoserranox@gmail.com>
2024-10-01 18:25:11 +0200

    commit

---
+++ Fundamentos2024/Form03DateTime.Designer.cs
@@ -0,0 +1,197 @@
+﻿namespace Fundamentos2024
+{
+    partial class Form03DateTime
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            txtFechaActual = new TextBox();
+            chkCambiarFormato = new CheckBox();
+            groupBox1 = new GroupBox();
+            rdbDias = new RadioButton();
+            rdbMeses = new RadioButton();
+            rdbAnyos = new RadioButton();
+            label2 = new Label();
+            txtIncremento = new TextBox();
+            btnIncrementar = new Button();
+            label3 = new Label();
+            txtNuevaFecha = new TextBox();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(30, 32);
+            label1.Name = "label1";
+            label1.Size = new Size(129, 30);
+            label1.TabIndex = 0;
+            label1.Text = "Fecha actual";
+            // 
+            // txtFechaActual
+            // 
+            txtFechaActual.Location = new Point(30, 65);
+            txtFechaActual.Name = "txtFechaActual";
+            txtFechaActual.Size = new Size(543, 35);
+            txtFechaActual.TabIndex = 1;
+            // 
+            // chkCambiarFormato
+            // 
+            chkCambiarFormato.AutoSize = true;
+            chkCambiarFormato.Location = new Point(32, 123);
+            chkCambiarFormato.Name = "chkCambiarFormato";
+            chkCambiarFormato.Size = new Size(246, 34);
+            chkCambiarFormato.TabIndex = 2;
+            chkCambiarFormato.Text = "Cambiar formato fecha";
+            chkCambiarFormato.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(btnIncrementar);
+            groupBox1.Controls.Add(txtIncremento);
+            groupBox1.Controls.Add(label2);
+            groupBox1.Controls.Add(rdbAnyos);
+            groupBox1.Controls.Add(rdbMeses);
+            groupBox1.Controls.Add(rdbDias);
+            groupBox1.Location = new Point(35, 171);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(495, 200);
+            groupBox1.TabIndex = 3;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "groupBox1";
+            // 
+            // rdbDias
+            // 
+            rdbDias.AutoSize = true;
+            rdbDias.Location = new Point(35, 42);
+            rdbDias.Name = "rdbDias";
+            rdbDias.Size = new Size(71, 34);
+            rdbDias.TabIndex = 0;
+            rdbDias.TabStop = true;
+            rdbDias.Text = "Días";
+            rdbDias.UseVisualStyleBackColor = true;
+            // 
+            // rdbMeses
+            // 
+            rdbMeses.AutoSize = true;
+            rdbMeses.Location = new Point(33, 82);
+            rdbMeses.Name = "rdbMeses";
+            rdbMeses.Size = new Size(90, 34);
+            rdbMeses.TabIndex = 1;
+            rdbMeses.TabStop = true;
+            rdbMeses.Text = "Meses";
+            rdbMeses.UseVisualStyleBackColor = true;
+            // 
+            // rdbAnyos
+            // 
+            rdbAnyos.AutoSize = true;
+            rdbAnyos.Location = new Point(35, 122);
+            rdbAnyos.Name = "rdbAnyos";
+            rdbAnyos.Size = new Size(78, 34);
+            rdbAnyos.TabIndex = 2;
+            rdbAnyos.TabStop = true;
+            rdbAnyos.Text = "Años";
+            rdbAnyos.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(267, 36);
+            label2.Name = "label2";
+            label2.Size = new Size(119, 30);
+            label2.TabIndex = 3;
+            label2.Text = "Incremento";
+            // 
+            // txtIncremento
+            // 
+            txtIncremento.Location = new Point(268, 69);
+            txtIncremento.Name = "txtIncremento";
+            txtIncremento.Size = new Size(183, 35);
+            txtIncremento.TabIndex = 4;
+            // 
+            // btnIncrementar
+            // 
+            btnIncrementar.Location = new Point(268, 118);
+            btnIncrementar.Name = "btnIncrementar";
+            btnIncrementar.Size = new Size(183, 38);
+            btnIncrementar.TabIndex = 5;
+            btnIncrementar.Text = "Incrementar";
+            btnIncrementar.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(35, 386);
+            label3.Name = "label3";
+            label3.Size = new Size(130, 30);
+            label3.TabIndex = 4;
+            label3.Text = "Nueva fecha";
+            // 
+            // txtNuevaFecha
+            // 
+            txtNuevaFecha.Location = new Point(38, 425);
+            txtNuevaFecha.Name = "txtNuevaFecha";
+            txtNuevaFecha.Size = new Size(535, 35);
+            txtNuevaFecha.TabIndex = 5;
+            // 
+            // Form03DateTime
+            // 
+            AutoScaleDimensions = new SizeF(12F, 30F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(645, 494);
+            Controls.Add(txtNuevaFecha);
+            Controls.Add(label3);
+            Controls.Add(groupBox1);
+            Controls.Add(chkCambiarFormato);
+            Controls.Add(txtFechaActual);
+            Controls.Add(label1);
+            Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            Margin = new Padding(5, 6, 5, 6);
+            Name = "Form03DateTime";
+            Text = "Form03DateTime";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox txtFechaActual;
+        private CheckBox chkCambiarFormato;
+        private GroupBox groupBox1;
+        private Button btnIncrementar;
+        private TextBox txtIncremento;
+        private Label label2;
+        private RadioButton rdbAnyos;
+        private RadioButton rdbMeses;
+        private RadioButton rdbDias;
+        private Label label3;
+        private TextBox txtNuevaFecha;
+    }
+}
--- Fundamentos2024/Program.cs
+++ Fundamentos2024/Program.cs
@@ -11,7 +11,7 @@ namespace Fundamentos2024
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form01SumarNumeros());
+            Application.Run(new Form03DateTime());
         }
     }
 }
