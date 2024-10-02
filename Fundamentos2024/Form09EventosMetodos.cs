f1f6632a264215ca6e0412c47b008c9ad0536bd9
serraguti <pacoserranox@gmail.com>
2024-10-02 19:15:49 +0200

    fdsfds

--- Fundamentos2024/Form09EventosMetodos.cs
+++ Fundamentos2024/Form09EventosMetodos.cs
@@ -15,6 +15,15 @@ namespace Fundamentos2024
         public Form09EventosMetodos()
         {
             InitializeComponent();
+            this.button1.Click += BotonPulsado;
+            this.button2.Click += BotonPulsado;
+            this.button3.Click += BotonPulsado;
+        }
+
+        //CREAMOS UN METODO DE EVENTO
+        void BotonPulsado(object sender, EventArgs e)
+        {
+            MessageBox.Show("Boton pulsado!!!!");
         }
     }
 }
