197c583a640a562669c092518a91780b55eeb7e1
serraguti <pacoserranox@gmail.com>
2024-10-02 21:38:17 +0200

    dsaadsad

---
+++ Fundamentos2024/Form11TablaMultiplicar.Designer.cs
@@ -0,0 +1,313 @@
+﻿namespace Fundamentos2024
+{
+    partial class Form11TablaMultiplicar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            button11 = new Button();
+            button12 = new Button();
+            button13 = new Button();
+            button14 = new Button();
+            button15 = new Button();
+            button16 = new Button();
+            label1 = new Label();
+            lstTablaMultiplicar = new ListBox();
+            button17 = new Button();
+            button18 = new Button();
+            button19 = new Button();
+            button20 = new Button();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(47, 32);
+            button1.Name = "button1";
+            button1.Size = new Size(63, 46);
+            button1.TabIndex = 0;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(125, 33);
+            button2.Name = "button2";
+            button2.Size = new Size(67, 45);
+            button2.TabIndex = 1;
+            button2.Text = "button2";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            button3.Location = new Point(286, 34);
+            button3.Name = "button3";
+            button3.Size = new Size(67, 45);
+            button3.TabIndex = 3;
+            button3.Text = "button3";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            button4.Location = new Point(208, 33);
+            button4.Name = "button4";
+            button4.Size = new Size(63, 46);
+            button4.TabIndex = 2;
+            button4.Text = "button4";
+            button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            button5.Location = new Point(125, 109);
+            button5.Name = "button5";
+            button5.Size = new Size(67, 45);
+            button5.TabIndex = 5;
+            button5.Text = "button5";
+            button5.UseVisualStyleBackColor = true;
+            // 
+            // button6
+            // 
+            button6.Location = new Point(47, 108);
+            button6.Name = "button6";
+            button6.Size = new Size(63, 46);
+            button6.TabIndex = 4;
+            button6.Text = "button6";
+            button6.UseVisualStyleBackColor = true;
+            // 
+            // button7
+            // 
+            button7.Location = new Point(286, 109);
+            button7.Name = "button7";
+            button7.Size = new Size(67, 45);
+            button7.TabIndex = 7;
+            button7.Text = "button7";
+            button7.UseVisualStyleBackColor = true;
+            // 
+            // button8
+            // 
+            button8.Location = new Point(208, 108);
+            button8.Name = "button8";
+            button8.Size = new Size(63, 46);
+            button8.TabIndex = 6;
+            button8.Text = "button8";
+            button8.UseVisualStyleBackColor = true;
+            // 
+            // button9
+            // 
+            button9.Location = new Point(286, 252);
+            button9.Name = "button9";
+            button9.Size = new Size(67, 45);
+            button9.TabIndex = 15;
+            button9.Text = "button9";
+            button9.UseVisualStyleBackColor = true;
+            // 
+            // button10
+            // 
+            button10.Location = new Point(208, 251);
+            button10.Name = "button10";
+            button10.Size = new Size(63, 46);
+            button10.TabIndex = 14;
+            button10.Text = "button10";
+            button10.UseVisualStyleBackColor = true;
+            // 
+            // button11
+            // 
+            button11.Location = new Point(125, 252);
+            button11.Name = "button11";
+            button11.Size = new Size(67, 45);
+            button11.TabIndex = 13;
+            button11.Text = "button11";
+            button11.UseVisualStyleBackColor = true;
+            // 
+            // button12
+            // 
+            button12.Location = new Point(47, 251);
+            button12.Name = "button12";
+            button12.Size = new Size(63, 46);
+            button12.TabIndex = 12;
+            button12.Text = "button12";
+            button12.UseVisualStyleBackColor = true;
+            // 
+            // button13
+            // 
+            button13.Location = new Point(286, 177);
+            button13.Name = "button13";
+            button13.Size = new Size(67, 45);
+            button13.TabIndex = 11;
+            button13.Text = "button13";
+            button13.UseVisualStyleBackColor = true;
+            // 
+            // button14
+            // 
+            button14.Location = new Point(208, 176);
+            button14.Name = "button14";
+            button14.Size = new Size(63, 46);
+            button14.TabIndex = 10;
+            button14.Text = "button14";
+            button14.UseVisualStyleBackColor = true;
+            // 
+            // button15
+            // 
+            button15.Location = new Point(125, 176);
+            button15.Name = "button15";
+            button15.Size = new Size(67, 45);
+            button15.TabIndex = 9;
+            button15.Text = "button15";
+            button15.UseVisualStyleBackColor = true;
+            // 
+            // button16
+            // 
+            button16.Location = new Point(47, 175);
+            button16.Name = "button16";
+            button16.Size = new Size(63, 46);
+            button16.TabIndex = 8;
+            button16.Text = "button16";
+            button16.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(419, 18);
+            label1.Name = "label1";
+            label1.Size = new Size(193, 30);
+            label1.TabIndex = 16;
+            label1.Text = "Tabla de multiplicar";
+            // 
+            // lstTablaMultiplicar
+            // 
+            lstTablaMultiplicar.FormattingEnabled = true;
+            lstTablaMultiplicar.ItemHeight = 30;
+            lstTablaMultiplicar.Location = new Point(426, 56);
+            lstTablaMultiplicar.Name = "lstTablaMultiplicar";
+            lstTablaMultiplicar.Size = new Size(264, 304);
+            lstTablaMultiplicar.TabIndex = 17;
+            // 
+            // button17
+            // 
+            button17.Location = new Point(286, 320);
+            button17.Name = "button17";
+            button17.Size = new Size(67, 45);
+            button17.TabIndex = 21;
+            button17.Text = "button17";
+            button17.UseVisualStyleBackColor = true;
+            // 
+            // button18
+            // 
+            button18.Location = new Point(208, 319);
+            button18.Name = "button18";
+            button18.Size = new Size(63, 46);
+            button18.TabIndex = 20;
+            button18.Text = "button18";
+            button18.UseVisualStyleBackColor = true;
+            // 
+            // button19
+            // 
+            button19.Location = new Point(125, 320);
+            button19.Name = "button19";
+            button19.Size = new Size(67, 45);
+            button19.TabIndex = 19;
+            button19.Text = "button19";
+            button19.UseVisualStyleBackColor = true;
+            // 
+            // button20
+            // 
+            button20.Location = new Point(47, 319);
+            button20.Name = "button20";
+            button20.Size = new Size(63, 46);
+            button20.TabIndex = 18;
+            button20.Text = "button20";
+            button20.UseVisualStyleBackColor = true;
+            // 
+            // Form11TablaMultiplicar
+            // 
+            AutoScaleDimensions = new SizeF(12F, 30F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(763, 395);
+            Controls.Add(button17);
+            Controls.Add(button18);
+            Controls.Add(button19);
+            Controls.Add(button20);
+            Controls.Add(lstTablaMultiplicar);
+            Controls.Add(label1);
+            Controls.Add(button9);
+            Controls.Add(button10);
+            Controls.Add(button11);
+            Controls.Add(button12);
+            Controls.Add(button13);
+            Controls.Add(button14);
+            Controls.Add(button15);
+            Controls.Add(button16);
+            Controls.Add(button7);
+            Controls.Add(button8);
+            Controls.Add(button5);
+            Controls.Add(button6);
+            Controls.Add(button3);
+            Controls.Add(button4);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            Margin = new Padding(5, 6, 5, 6);
+            Name = "Form11TablaMultiplicar";
+            Text = "Form11TablaMultiplicar";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+        private Button button11;
+        private Button button12;
+        private Button button13;
+        private Button button14;
+        private Button button15;
+        private Button button16;
+        private Label label1;
+        private ListBox lstTablaMultiplicar;
+        private Button button17;
+        private Button button18;
+        private Button button19;
+        private Button button20;
+    }
+}
---
+++ Fundamentos2024/Form11TablaMultiplicar.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Fundamentos2024
+{
+    public partial class Form11TablaMultiplicar : Form
+    {
+        public Form11TablaMultiplicar()
+        {
+            InitializeComponent();
+            Random random = new Random();
+            foreach (Control miControl in this.Controls)
+            {
+                if (miControl is Button)
+                {
+                    int numeroAleatorio = random.Next(1, 40);
+                    miControl.Text = numeroAleatorio.ToString();
+                    miControl.Click += TablaMultiplicar;
+                }
+            }
+        }
+
+        void TablaMultiplicar(object sender, EventArgs e)
+        {
+            Button boton = (Button)sender;
+            int numeroBoton = int.Parse(boton.Text);
+            this.lstTablaMultiplicar.Items.Add(numeroBoton);
+        }
+    }
+}
