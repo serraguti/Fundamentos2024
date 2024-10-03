45e9408c7cdf35136f18cea4ff3f001f565bf74a
serraguti <pacoserranox@gmail.com>
2024-10-03 21:22:14 +0200

    cccc

---
+++ Fundamentos2024/Form13Files.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Fundamentos2024
+{
+    partial class Form13Files
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            textBox1 = new TextBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(30, 22);
+            label1.Name = "label1";
+            label1.Size = new Size(179, 30);
+            label1.TabIndex = 0;
+            label1.Text = "Contenido fichero";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(33, 58);
+            textBox1.Multiline = true;
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(468, 369);
+            textBox1.TabIndex = 1;
+            // 
+            // Form13Files
+            // 
+            AutoScaleDimensions = new SizeF(12F, 30F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(875, 476);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            Margin = new Padding(5, 6, 5, 6);
+            Name = "Form13Files";
+            Text = "Form13Files";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+    }
+}
