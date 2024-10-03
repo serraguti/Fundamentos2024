474e72dc395b109f7275030cbcb4fdad65b7d614
serraguti <pacoserranox@gmail.com>
2024-10-03 21:53:53 +0200

    fdsfds

--- Fundamentos2024/Form13Files.cs
+++ Fundamentos2024/Form13Files.cs
@@ -22,19 +22,19 @@ namespace Fundamentos2024
         {
             //NECESITAMOS LA RUTA DEL FICHERO
             this.openFileDialog1.ShowDialog();
-
-            //FileInfo file = new FileInfo(this.rutaFichero);
-            ////CON LA CLASE TextReader LEEMOS TEXTO PLANO DE UN FICHERO
-            //using (TextReader reader = file.OpenText())
-            //{
-            //    //SI TENEMOS METODOS Async PARA LEER, DEBEMOS UTILIZARLOS
-            //    //LOS METODOS ASYNC FINALIZAN CON ESA PALABRA
-            //    //LEEMOS EL CONTENIDO DEL FILE
-            //    string contenido = await reader.ReadToEndAsync();
-            //    //DEBEMOS CERRAR EL FICHERO
-            //    reader.Close();
-            //    this.txtContenido.Text = contenido;
-            //}
+            string path = this.openFileDialog1.FileName;
+            FileInfo file = new FileInfo(path);
+            //CON LA CLASE TextReader LEEMOS TEXTO PLANO DE UN FICHERO
+            using (TextReader reader = file.OpenText())
+            {
+                //SI TENEMOS METODOS Async PARA LEER, DEBEMOS UTILIZARLOS
+                //LOS METODOS ASYNC FINALIZAN CON ESA PALABRA
+                //LEEMOS EL CONTENIDO DEL FILE
+                string contenido = await reader.ReadToEndAsync();
+                //DEBEMOS CERRAR EL FICHERO
+                reader.Close();
+                this.txtContenido.Text = contenido;
+            }
         }
 
         private async void btnGuardarFichero_Click(object sender, EventArgs e)
