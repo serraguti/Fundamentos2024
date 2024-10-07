6172b578e8212ba9c97747ed913983617996ef50
serraguti <pacoserranox@gmail.com>
2024-10-07 19:04:56 +0200

    dsa

---
+++ ProyectoClases/Mascota.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProyectoClases
+{
+    public class Mascota
+    {
+        public string Nombre { get; set; }
+        public string Raza { get; set; }
+        public int Anyos { get; set; }
+    }
+}
