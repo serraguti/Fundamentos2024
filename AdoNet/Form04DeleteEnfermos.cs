3571ea93f97a0291169837594905f4164ba2c122
serraguti <pacoserranox@gmail.com>
2024-10-09 21:35:22 +0200

    fdsfd

--- AdoNet/Form04DeleteEnfermos.cs
+++ AdoNet/Form04DeleteEnfermos.cs
@@ -23,13 +23,23 @@ namespace AdoNet
             InitializeComponent();
             this.connectionString = @"Data Source=LOCALHOST\SQLEXPRESS;Initial Catalog=HOSPITAL;User ID=SA;Persist Security Info=True;";
             //this.connectionString = @"Data Source=LOCALHOST;Initial Catalog=HOSPITAL;User ID=SA;Persist Security Info=True;";
-
+            this.cn = new SqlConnection(this.connectionString);
+            this.com = new SqlCommand();
         }
 
         private void btnEliminar_Click(object sender, EventArgs e)
         {
             //AQUI UNA CONSULTA PARA ELIMINAR UN ENFERMO POR SU INSCRIPCION
             //MOSTRAMOS EN EL LABEL EL NUMERO DE ENFERMOS ELIMINADOS
+            string inscripcion = this.txtInscripcion.Text;
+            string sql = "delete from ENFERMO where INSCRIPCION=" + inscripcion;
+            this.com.Connection = this.cn;
+            this.com.CommandType = CommandType.Text;
+            this.com.CommandText = sql;
+            this.cn.Open();
+            int registrosEliminados = this.com.ExecuteNonQuery();
+            this.cn.Close();
+            this.lblMensaje.Text = "Enfermos eliminados: " + registrosEliminados;
         }
     }
 }
