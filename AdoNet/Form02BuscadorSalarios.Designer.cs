8d4fbfabdaa756d73e027e3ea8af82ccb44c0054
serraguti <pacoserranox@gmail.com>
2024-10-09 20:42:47 +0200

    dsa

---
+++ AdoNet/Form02BuscadorSalarios.Designer.cs
@@ -0,0 +1,108 @@
+﻿namespace AdoNet
+{
+    partial class Form02BuscadorSalarios
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            txtSalario = new TextBox();
+            btnBuscarEmpleados = new Button();
+            label2 = new Label();
+            lstEmpleados = new ListBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(27, 20);
+            label1.Name = "label1";
+            label1.Size = new Size(183, 30);
+            label1.TabIndex = 0;
+            label1.Text = "Introduzca salario:";
+            // 
+            // txtSalario
+            // 
+            txtSalario.Location = new Point(32, 54);
+            txtSalario.Name = "txtSalario";
+            txtSalario.Size = new Size(324, 35);
+            txtSalario.TabIndex = 1;
+            // 
+            // btnBuscarEmpleados
+            // 
+            btnBuscarEmpleados.Location = new Point(35, 103);
+            btnBuscarEmpleados.Name = "btnBuscarEmpleados";
+            btnBuscarEmpleados.Size = new Size(321, 48);
+            btnBuscarEmpleados.TabIndex = 2;
+            btnBuscarEmpleados.Text = "Buscar empleados";
+            btnBuscarEmpleados.UseVisualStyleBackColor = true;
+            btnBuscarEmpleados.Click += btnBuscarEmpleados_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(34, 161);
+            label2.Name = "label2";
+            label2.Size = new Size(114, 30);
+            label2.TabIndex = 3;
+            label2.Text = "Empleados";
+            // 
+            // lstEmpleados
+            // 
+            lstEmpleados.FormattingEnabled = true;
+            lstEmpleados.ItemHeight = 30;
+            lstEmpleados.Location = new Point(40, 198);
+            lstEmpleados.Name = "lstEmpleados";
+            lstEmpleados.Size = new Size(316, 274);
+            lstEmpleados.TabIndex = 4;
+            // 
+            // Form02BuscadorSalarios
+            // 
+            AutoScaleDimensions = new SizeF(12F, 30F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(396, 492);
+            Controls.Add(lstEmpleados);
+            Controls.Add(label2);
+            Controls.Add(btnBuscarEmpleados);
+            Controls.Add(txtSalario);
+            Controls.Add(label1);
+            Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            Margin = new Padding(5, 6, 5, 6);
+            Name = "Form02BuscadorSalarios";
+            Text = "Form02BuscadorSalarios";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox txtSalario;
+        private Button btnBuscarEmpleados;
+        private Label label2;
+        private ListBox lstEmpleados;
+    }
+}
---
+++ AdoNet/Form02BuscadorSalarios.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace AdoNet
+{
+    public partial class Form02BuscadorSalarios : Form
+    {
+        string connectionString;
+        SqlConnection cn;
+        SqlCommand com;
+        SqlDataReader reader;
+
+        public Form02BuscadorSalarios()
+        {
+            InitializeComponent();
+            //this.connectionString = @"Data Source=LOCALHOST\SQLEXPRESS;Initial Catalog=HOSPITAL;User ID=SA;Persist Security Info=True;";
+            this.connectionString = @"Data Source=LOCALHOST;Initial Catalog=HOSPITAL;User ID=SA;Persist Security Info=True;";
+            this.cn = new SqlConnection(this.connectionString);
+            this.com = new SqlCommand();
+        }
+
+        private void btnBuscarEmpleados_Click(object sender, EventArgs e)
+        {
+            string sql = "select * from EMP where SALARIO >= " + this.txtSalario.Text;
+            this.com.Connection = this.cn;
+            this.com.CommandType = CommandType.Text;
+            this.com.CommandText = sql;
+            //ABRIMOS CONEXION
+            this.cn.Open();
+            //EJECUTAMOS EL COMANDO CON LA CONSULTA
+            this.reader = this.com.ExecuteReader();
+            //LIMPIAMOS LA LISTA VISUAL
+            this.lstEmpleados.Items.Clear();
+            //RECORREMOS EL LECTOR FILA A FILA
+            while (this.reader.Read())
+            {
+                //EXTRAEMOS LOS DATOS QUE NECESITEMOS
+                string apellido = this.reader["APELLIDO"].ToString();
+                string salario = this.reader["SALARIO"].ToString();
+                //DIBUJAMOS LOS DATOS
+                this.lstEmpleados.Items.Add(apellido + " - " + salario);
+            }
+            //CERRAMOS TODOS LOS RECURSOS PARA PODER REUTILIZARLOS
+            this.reader.Close();
+            this.cn.Close();
+        }
+    }
+}
