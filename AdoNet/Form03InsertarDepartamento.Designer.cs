969578dc2caedacdb2673b8e9ad74cb4563f2ac8
serraguti <pacoserranox@gmail.com>
2024-10-09 20:52:12 +0200

    fdsfds

---
+++ AdoNet/Form03InsertarDepartamento.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace AdoNet
+{
+    partial class Form03InsertarDepartamento
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            txtNumero = new TextBox();
+            label2 = new Label();
+            txtNombre = new TextBox();
+            label3 = new Label();
+            txtLocalidad = new TextBox();
+            btnInsertarDepartamento = new Button();
+            lblMensaje = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(36, 17);
+            label1.Name = "label1";
+            label1.Size = new Size(89, 30);
+            label1.TabIndex = 0;
+            label1.Text = "Número";
+            // 
+            // txtNumero
+            // 
+            txtNumero.Location = new Point(41, 50);
+            txtNumero.Name = "txtNumero";
+            txtNumero.Size = new Size(100, 35);
+            txtNumero.TabIndex = 1;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(41, 100);
+            label2.Name = "label2";
+            label2.Size = new Size(89, 30);
+            label2.TabIndex = 2;
+            label2.Text = "Nombre";
+            // 
+            // txtNombre
+            // 
+            txtNombre.Location = new Point(45, 132);
+            txtNombre.Name = "txtNombre";
+            txtNombre.Size = new Size(293, 35);
+            txtNombre.TabIndex = 3;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(44, 181);
+            label3.Name = "label3";
+            label3.Size = new Size(101, 30);
+            label3.TabIndex = 4;
+            label3.Text = "Localidad";
+            // 
+            // txtLocalidad
+            // 
+            txtLocalidad.Location = new Point(46, 218);
+            txtLocalidad.Name = "txtLocalidad";
+            txtLocalidad.Size = new Size(292, 35);
+            txtLocalidad.TabIndex = 5;
+            // 
+            // btnInsertarDepartamento
+            // 
+            btnInsertarDepartamento.Location = new Point(50, 282);
+            btnInsertarDepartamento.Name = "btnInsertarDepartamento";
+            btnInsertarDepartamento.Size = new Size(397, 60);
+            btnInsertarDepartamento.TabIndex = 6;
+            btnInsertarDepartamento.Text = "Insertar Departamento";
+            btnInsertarDepartamento.UseVisualStyleBackColor = true;
+            // 
+            // lblMensaje
+            // 
+            lblMensaje.AutoSize = true;
+            lblMensaje.Location = new Point(50, 363);
+            lblMensaje.Name = "lblMensaje";
+            lblMensaje.Size = new Size(113, 30);
+            lblMensaje.TabIndex = 7;
+            lblMensaje.Text = "lblMensaje";
+            // 
+            // Form03InsertarDepartamento
+            // 
+            AutoScaleDimensions = new SizeF(12F, 30F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(499, 440);
+            Controls.Add(lblMensaje);
+            Controls.Add(btnInsertarDepartamento);
+            Controls.Add(txtLocalidad);
+            Controls.Add(label3);
+            Controls.Add(txtNombre);
+            Controls.Add(label2);
+            Controls.Add(txtNumero);
+            Controls.Add(label1);
+            Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            Margin = new Padding(5, 6, 5, 6);
+            Name = "Form03InsertarDepartamento";
+            Text = "Form03InsertarDepartamento";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox txtNumero;
+        private Label label2;
+        private TextBox txtNombre;
+        private Label label3;
+        private TextBox txtLocalidad;
+        private Button btnInsertarDepartamento;
+        private Label lblMensaje;
+    }
+}
--- AdoNet/Program.cs
+++ AdoNet/Program.cs
@@ -11,7 +11,7 @@ namespace AdoNet
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new Form02BuscadorSalarios());
+            Application.Run(new Form03InsertarDepartamento());
         }
     }
 }
