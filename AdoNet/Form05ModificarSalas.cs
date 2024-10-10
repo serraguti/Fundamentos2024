679302f5e411d678c1f4437cd9247e13f3efe233
serraguti <pacoserranox@gmail.com>
2024-10-10 18:46:26 +0200

    dfs

--- AdoNet/Form05ModificarSalas.cs
+++ AdoNet/Form05ModificarSalas.cs
@@ -7,14 +7,45 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace AdoNet
 {
     public partial class Form05ModificarSalas : Form
     {
+        string connectionString;
+        SqlConnection cn;
+        SqlCommand com;
+        SqlDataReader reader;
+
         public Form05ModificarSalas()
         {
             InitializeComponent();
+            //this.connectionString = @"Data Source=LOCALHOST\SQLEXPRESS;Initial Catalog=HOSPITAL;User ID=SA;Persist Security Info=True;";
+            this.connectionString = @"Data Source=LOCALHOST;Initial Catalog=HOSPITAL;User ID=SA;Persist Security Info=True;";
+            this.cn = new SqlConnection(this.connectionString);
+            this.com = new SqlCommand();
+            //DIBUJAR LOS NOMBRES DE SALA
+            this.CargarSalas();
         }
+
+        public void CargarSalas()
+        {
+            string sql = "select * from sala";
+            this.com.Connection = this.cn;
+            this.com.CommandType = CommandType.Text;
+            this.com.CommandText = sql;
+            this.cn.Open();
+            this.reader = this.com.ExecuteReader();
+            this.lstSalas.Items.Clear();
+            while (this.reader.Read())
+            {
+                string nombre = this.reader["NOMBRE"].ToString();
+                this.lstSalas.Items.Add(nombre);
+            }
+            this.reader.Close();
+            this.cn.Close();
+        }
+
     }
 }
