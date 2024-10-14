485a6f5f1608f7d84c6f832fcaa45a279ed72d7e
serraguti <pacoserranox@gmail.com>
2024-10-14 20:30:52 +0200

    ddd

--- AdoNet/Form10HospitalesDoctores.cs
+++ AdoNet/Form10HospitalesDoctores.cs
@@ -7,14 +7,37 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using AdoNet.Models;
+using AdoNet.Repositories;
 
 namespace AdoNet
 {
     public partial class Form10HospitalesDoctores : Form
     {
+        private RepositoryHospital repo;
+
         public Form10HospitalesDoctores()
         {
             InitializeComponent();
+            this.repo = new RepositoryHospital();
+            this.CargarHospitales();
+        }
+
+        private void CargarHospitales() {
+            List<Hospital> hospitales = this.repo.GetHospitales();
+            this.lsvHospitales.Items.Clear();
+            foreach (Hospital hospital in hospitales)
+            {
+                //CREAMOS UN ListViewItem
+                ListViewItem item = new ListViewItem();
+                item.Text = hospital.IdHospital.ToString();
+                item.SubItems.Add(hospital.Nombre);
+                item.SubItems.Add(hospital.Direccion);
+                item.SubItems.Add(hospital.Telefono);
+                item.SubItems.Add(hospital.Camas.ToString());
+                //AGREGAMOS EL Item A LA LISTA DEL DIBUJO
+                this.lsvHospitales.Items.Add(item);
+            }
         }
     }
 }
