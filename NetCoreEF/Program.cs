e29a9db95efe7e27a3e579b8a9bfe1cedebd3cc6
serraguti <pacoserranox@gmail.com>
2024-10-15 21:21:52 +0200

    fdsfds

--- NetCoreEF/Program.cs
+++ NetCoreEF/Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.Extensions.DependencyInjection;
+using NetCoreEF.Data;
+
 namespace NetCoreEF
 {
     internal static class Program
@@ -11,6 +14,20 @@ namespace NetCoreEF
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
+            //ANTES DE LANZAR EL FORMULARIO, VAMOS A REALIZAR TODAS LAS INYECCIONES NECESARIAS
+            //this.connectionString = @"Data Source=LOCALHOST\SQLEXPRESS;Initial Catalog=HOSPITAL;User ID=SA;Persist Security Info=True;";
+            string connectionString = @"Data Source=LOCALHOST;Initial Catalog=HOSPITAL;User ID=SA;Persist Security Info=True;";
+            //NECESITAMOS UN OBJETO LLAMADO ServiceProvider QUE ES EL ENCARGADO
+            //DE RESOLVER LAS DEPENDENCIAS EN LAS CLASES
+            //DEBEMOS INDICAR CADA CLASE QUE UTILIZAREMOS
+            //1) SI SON CLASES "NORMALES" SE UTILIZA AddTransient()
+            //2) CLASES DBCONTEXT SE DEBEN ENVIAR MEDIANTE AddDbContext()
+            ServiceProvider provider = new ServiceCollection()
+                .AddTransient<RepositoryEmpleados>()
+                .AddDbContext<EmpleadosContext>()
+                .BuildServiceProvider();
+
+            provider.GetService<RepositoryEmpleados>();
             Application.Run(new Form1());
         }
     }
