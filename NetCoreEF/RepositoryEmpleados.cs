a5e4e92693cd595d8b4151dd795ded5c8c30f969
serraguti <pacoserranox@gmail.com>
2024-10-15 21:05:16 +0200

    cxdcs

---
+++ NetCoreEF/RepositoryEmpleados.cs
@@ -0,0 +1,32 @@
+﻿using NetCoreEF.Data;
+using NetCoreEF.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetCoreEF
+{
+    public class RepositoryEmpleados
+    {
+        //ESTA CLASE DEBE UTILIZAR EL DBCONTEXT PARA LAS CONSULTAS
+        private EmpleadosContext context;
+
+        //EL OBJETO CONTEXT NO LO CREAREMOS NOSOTROS AQUI, SINO 
+        //QUE NOS LO PROPORCIONARA NUESTRO CONTAINER MEDIANTE INYECCION
+        //DE DEPENDENCIAS
+        public RepositoryEmpleados(EmpleadosContext context)
+        {
+            this.context = context;
+        }
+
+        public List<Empleado> GetEmpleados()
+        {
+            //CREAMOS UNA CONSULTA LINQ
+            var consulta = from datos in this.context.Empleados
+                           select datos;
+            return consulta.ToList();
+        }
+    }
+}
