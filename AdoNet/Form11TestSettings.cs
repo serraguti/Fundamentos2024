a00a6c975d0cfe37448c2131d8447280de1e59a7
serraguti <pacoserranox@gmail.com>
2024-10-15 19:13:02 +0200

    fdsfd

--- AdoNet/Form11TestSettings.cs
+++ AdoNet/Form11TestSettings.cs
@@ -20,13 +20,9 @@ namespace AdoNet
 
         private void btnSettings_Click(object sender, EventArgs e)
         {
-            ConfigurationBuilder builder = new ConfigurationBuilder();
-            //DEBEMOS INDICAR DONDE TENEMOS LA CADENA DE CONEXION
-            //EL FICHERO Y SU NOMBRE (appsettings.json)
-            builder.SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json", false, true);
-            //CONSTRUIMOS EL FICHERO PARA NUESTRA APP
-            IConfigurationRoot configuration = builder.Build();
+            IConfigurationRoot configuration = Program.GetConfiguration();
+
+
             //SIMPLEMENTE TENEMOS QUE RECUPERAR LOS DATOS DE NUESTRO FICHERO
             //DENTRO DEL FICHERO, TENEMOS ZONAS CONOCIDAS Y ZONAS INVENTADAS
             //SI ES UNA ZONA CONOCIDA COMO ConnectionStrings SE UTILIZA EL 
--- AdoNet/Program.cs
+++ AdoNet/Program.cs
@@ -1,7 +1,36 @@
+using Microsoft.Extensions.Configuration;
+
 namespace AdoNet
 {
     internal static class Program
     {
+        //PARA PODER ACCEDER A LOS ELEMENTOS DEBEMOS DECLARAR 
+        //LOS METODOS CON LA PALABRA static
+        public static IConfigurationRoot GetConfiguration()
+        {
+            ConfigurationBuilder builder = new ConfigurationBuilder();
+            //DEBEMOS INDICAR DONDE TENEMOS LA CADENA DE CONEXION
+            //EL FICHERO Y SU NOMBRE (appsettings.json)
+            builder.SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", false, true);
+            //CONSTRUIMOS EL FICHERO PARA NUESTRA APP
+            IConfigurationRoot configuration = builder.Build();
+            return configuration;
+        }
+
+        public static string GetConnectionString()
+        {
+            ConfigurationBuilder builder = new ConfigurationBuilder();
+            //DEBEMOS INDICAR DONDE TENEMOS LA CADENA DE CONEXION
+            //EL FICHERO Y SU NOMBRE (appsettings.json)
+            builder.SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", false, true);
+            //CONSTRUIMOS EL FICHERO PARA NUESTRA APP
+            IConfigurationRoot configuration = builder.Build();
+            string connectionString = configuration.GetConnectionString("HospitalExpress");
+            return connectionString;
+        }
+
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
