55649ec167e6d3a0b42290916af0d09afdae301c
serraguti <pacoserranox@gmail.com>
2024-10-15 21:43:15 +0200

    fdsfd

--- NetCoreEF/Form01TodosEmpleados.cs
+++ NetCoreEF/Form01TodosEmpleados.cs
@@ -14,16 +14,18 @@ namespace NetCoreEF
 {
     public partial class Form01TodosEmpleados : Form
     {
+        RepositoryEmpleados repo;
+
         public Form01TodosEmpleados()
         {
             InitializeComponent();
+            //RECUPERAMOS EL REPOSITORIO DEL PROVIDER DE PROGRAM
+            this.repo =
+                Program.provider.GetService<RepositoryEmpleados>();
         }
 
         private void btnCargarEmpleados_Click(object sender, EventArgs e)
         {
-            //RECUPERAMOS EL REPOSITORIO DEL PROVIDER DE PROGRAM
-            RepositoryEmpleados repo = 
-                Program.provider.GetService<RepositoryEmpleados>();
             //RECUPERAMOS LOS EMPLEADOS DE LA BASE DE DATOS
             List<Empleado> empleados = repo.GetEmpleados();
             //DIBUJAMOS LOS EMPLEADOS EN LA LISTA
