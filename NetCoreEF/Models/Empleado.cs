e297e4714ca1b640bce35634f56685bcf24a01a4
serraguti <pacoserranox@gmail.com>
2024-10-15 20:37:11 +0200

    gfdgfd

--- NetCoreEF/Models/Empleado.cs
+++ NetCoreEF/Models/Empleado.cs
@@ -1,12 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace NetCoreEF.Models
 {
+    //INDICAMOS QUE UTILICE LA TABLA EMP DE LA BASE DE DATOS
+    [Table("EMP")]
     public class Empleado
     {
+        [Key]
+        [Column("EMP_NO")]
+        public int IdEmpleado { get; set; }
+        [Column("APELLIDO")]
+        public string Apellido { get; set; }
+        [Column("OFICIO")]
+        public string Oficio { get; set; }
+        [Column("SALARIO")]
+        public int Salario { get; set; }
     }
 }
