19db498811da73b2e14c22d439acf9553eccdfbb
serraguti <pacoserranox@gmail.com>
2024-10-16 19:25:56 +0200

    dssds

--- NetCoreEF/Models/Hospital.cs
+++ NetCoreEF/Models/Hospital.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,7 @@ namespace NetCoreEF.Models
     [Table("HOSPITAL")]
     public class Hospital
     {
+        [Key]
         [Column("HOSPITAL_COD")]
         public int IdHospital { get; set; }
         [Column("NOMBRE")]
