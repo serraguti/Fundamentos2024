ea0689501959623731d8cea38c94f24f79632d3f
serraguti <pacoserranox@gmail.com>
2024-10-16 19:12:20 +0200

    sd

---
+++ NetCoreEF/Models/Hospital.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetCoreEF.Models
+{
+    [Table("HOSPITAL")]
+    public class Hospital
+    {
+        [Column("HOSPITAL_COD")]
+        public int IdHospital { get; set; }
+        [Column("NOMBRE")]
+        public string Nombre { get; set; }
+        [Column("DIRECCION")]
+        public string Direccion { get; set; }
+        [Column("TELEFONO")]
+        public string Telefono { get; set; }
+        [Column("NUM_CAMA")]
+        public int Camas { get; set; }
+    }
+}
