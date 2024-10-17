467005154d7a530400bee736d4c4548905690bba
serraguti <pacoserranox@gmail.com>
2024-10-17 18:36:44 +0200

    aaa

--- NetCoreEF/Form05DoctoresPracticaPaco.Designer.cs
+++ NetCoreEF/Form05DoctoresPracticaPaco.Designer.cs
@@ -28,18 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
+            label1 = new Label();
+            lstDoctores = new ListBox();
+            btnCargarDoctores = new Button();
             SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(24, 12);
+            label1.Name = "label1";
+            label1.Size = new Size(96, 30);
+            label1.TabIndex = 0;
+            label1.Text = "Doctores";
+            // 
+            // lstDoctores
+            // 
+            lstDoctores.FormattingEnabled = true;
+            lstDoctores.ItemHeight = 30;
+            lstDoctores.Location = new Point(24, 45);
+            lstDoctores.Name = "lstDoctores";
+            lstDoctores.Size = new Size(435, 364);
+            lstDoctores.TabIndex = 1;
+            // 
+            // btnCargarDoctores
+            // 
+            btnCargarDoctores.Location = new Point(540, 49);
+            btnCargarDoctores.Name = "btnCargarDoctores";
+            btnCargarDoctores.Size = new Size(236, 63);
+            btnCargarDoctores.TabIndex = 2;
+            btnCargarDoctores.Text = "Cargar Doctores";
+            btnCargarDoctores.UseVisualStyleBackColor = true;
+            btnCargarDoctores.Click += btnCargarDoctores_Click;
             // 
             // Form05DoctoresPracticaPaco
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(12F, 30F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(526, 347);
+            ClientSize = new Size(822, 436);
+            Controls.Add(btnCargarDoctores);
+            Controls.Add(lstDoctores);
+            Controls.Add(label1);
+            Font = new Font("Segoe UI", 15.75F, FontStyle.Regular, GraphicsUnit.Point, 0);
+            Margin = new Padding(5, 6, 5, 6);
             Name = "Form05DoctoresPracticaPaco";
             Text = "Form05DoctoresPracticaPaco";
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private ListBox lstDoctores;
+        private Button btnCargarDoctores;
     }
 }
--- NetCoreEF/Form05DoctoresPracticaPaco.cs
+++ NetCoreEF/Form05DoctoresPracticaPaco.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using NetCoreEF.Models;
+using NetCoreEF.Repositories;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +15,23 @@ namespace NetCoreEF
 {
     public partial class Form05DoctoresPracticaPaco : Form
     {
+        private RepositoryDoctores repo;
+
         public Form05DoctoresPracticaPaco()
         {
             InitializeComponent();
+            this.repo =
+                Program.provider.GetService<RepositoryDoctores>();
+        }
+
+        private void btnCargarDoctores_Click(object sender, EventArgs e)
+        {
+            List<Doctor> doctores = this.repo.GetDoctores();
+            this.lstDoctores.Items.Clear();
+            foreach (Doctor doc in doctores)
+            {
+                this.lstDoctores.Items.Add(doc.Apellido + " - " + doc.Especialidad);
+            }
         }
     }
 }
